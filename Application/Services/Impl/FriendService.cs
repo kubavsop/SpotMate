3d46705ce22ef39fd5ac45b7ddc45e285a283c26
maksim <mazhur04@gmail.com>
2024-06-10 17:16:20 +0700

    Add friend service

--- Application/Services/Impl/FriendService.cs
+++ Application/Services/Impl/FriendService.cs
@@ -1,19 +1,54 @@
-﻿using SpotMate.Application.DTOs.Requests;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SpotMate.Application.Context;
+using SpotMate.Application.DTOs.Requests;
 using SpotMate.Application.DTOs.Responses;
+using SpotMate.Application.Exceptions;
 using SpotMate.Application.OperationResult;
 
 namespace SpotMate.Application.Services.Impl;
 
 public sealed class FriendService: IFriendService
 {
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
 
-    public Task<Result<IEnumerable<UserShortDto>>> GetFriendsAsync(UserShortSearchParameters userShortSearchParameters, Guid userId)
+    public FriendService(IApplicationDbContext context, IMapper mapper)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _mapper = mapper;
     }
 
-    public Task<Result> DeleteFriendAsync(Guid friendId, Guid userId)
+    public async Task<Result<IEnumerable<UserShortDto>>> GetFriendsAsync(UserShortSearchParameters userShortSearchParameters, Guid userId)
     {
-        throw new NotImplementedException();
+        var normalizedUserName = userShortSearchParameters.UserName?.ToUpper();
+        
+        var friends = await _context.UserFriends
+            .Include( uf => uf.FirstUser)
+            .Include(uf => uf.SecondUser)
+            .Where(uf => uf.SecondUserId == userId || uf.FirstUserId == userId)
+            .Where(uf => normalizedUserName == null ||
+                         (uf.FirstUserId == userId && uf.SecondUser.UserName == normalizedUserName) ||
+                         (uf.SecondUserId == userId && uf.FirstUser.UserName == normalizedUserName))
+            .Select(uf => uf.FirstUserId == userId ? uf.SecondUser : uf.FirstUser)
+            .ToListAsync();
+
+        return _mapper.Map<List<UserShortDto>>(friends);
+    }
+
+    public async Task<Result> DeleteFriendAsync(Guid friendId, Guid userId)
+    {
+        var userFriend = await _context.UserFriends.FirstOrDefaultAsync(uf =>
+            (uf.FirstUserId == friendId && uf.SecondUserId == userId) ||
+            (uf.SecondUserId == friendId && uf.FirstUserId == userId));
+
+        if (userFriend == null)
+        {
+            return new BadRequestException("The user is not your friend");
+        }
+
+        _context.UserFriends.Remove(userFriend);
+        
+        return Result.Success();
     }
 }
