cdf2a6d442a171561829b50e37b54658acf77ac6
maksim <mazhur04@gmail.com>
2024-06-17 18:14:10 +0700

    Delete cors default policy

--- Web/DependencyInjection.cs
+++ Web/DependencyInjection.cs
@@ -26,17 +26,6 @@ public static class DependencyInjection
             options.LowercaseQueryStrings = true;
         });
         services.ConfigureOptions<SwaggerGenOptionsConfigure>();
-
-        services.AddCors(options =>
-        {
-            options.AddDefaultPolicy(policy =>
-            {
-                policy
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-            });
-        });
         
         return services;
     }
